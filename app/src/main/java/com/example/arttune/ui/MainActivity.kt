dfdc0ebe881c03e302314c0d710ed4715471fd44
Andrea Tongsak <atongsak@gmail.com>
2023-03-20 23:35:40 -0700

    Added view model

--- app/src/main/java/com/example/arttune/ui/MainActivity.kt
+++ app/src/main/java/com/example/arttune/ui/MainActivity.kt
@@ -2,19 +2,22 @@ package com.example.arttune.ui
 
 import android.os.Bundle
 import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.arttune.api.SpotifyService
+import com.example.githubsearchwithsettings.R
 import com.google.android.material.progressindicator.CircularProgressIndicator
 
 const val SPOTIFY_KEY = "9f02ab55a6bb4fdc8ab0e42a6208574a"
 
 class MainActivity : AppCompatActivity() {
     private val tag = "MainActivity"
-
+    private val spotifySearchViewModel: SpotifySearchViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
     }
     override fun onResume() {
