7930c6de3be4320834cbc94320646f3ee6c0e98e
dodsonta <71948722+dodsonta@users.noreply.github.com>
2023-03-21 17:22:24 -0700

    Create basic activity and icon

---
+++ app/src/main/java/com/example/arttune/ui/GalleryActivity.kt
@@ -0,0 +1,6 @@
+package com.example.arttune.ui
+
+import androidx.appcompat.app.AppCompatActivity
+
+class GalleryActivity: AppCompatActivity() {
+}
--- app/src/main/java/com/example/arttune/ui/MainActivity.kt
+++ app/src/main/java/com/example/arttune/ui/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.arttune.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.example.arttune.R
 import com.example.arttune.api.SpotifyService
-import com.example.githubsearchwithsettings.R
 import com.google.android.material.progressindicator.CircularProgressIndicator
 
 const val SPOTIFY_KEY = "9f02ab55a6bb4fdc8ab0e42a6208574a"
@@ -28,5 +31,19 @@ class MainActivity : AppCompatActivity() {
          */
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_main, menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.action_gallery -> {
+                val intent = Intent(this, GalleryActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else ->super.onOptionsItemSelected(item)
+        }
+    }
 }
