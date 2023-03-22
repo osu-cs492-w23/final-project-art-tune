553e5edb7b1394c05cea9ac4d038b5a2eaacd226
dodsonta <71948722+dodsonta@users.noreply.github.com>
2023-03-21 21:20:03 -0700

    Placeholder added ok

--- app/src/main/java/com/example/arttune/ui/GalleryAdapter.kt
+++ app/src/main/java/com/example/arttune/ui/GalleryAdapter.kt
@@ -1,4 +1,37 @@
 package com.example.arttune.ui
 
-class GalleryAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.arttune.R
+import com.example.arttune.data.SavedPiece
+
+class GalleryAdapter: RecyclerView.Adapter<GalleryAdapter.ViewHolder>() {
+    private var gallery: List<SavedPiece> = listOf()
+    override fun getItemCount(): Int = this.gallery.size
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.gallery_list_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(this.gallery[position])
+    }
+
+    fun updateGallery(){
+        gallery = listOf(SavedPiece("On GP", "Death Grips"))
+        notifyDataSetChanged()
+    }
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        private val songName: TextView = itemView.findViewById(R.id.tv_song_name)
+        private val artistName: TextView = itemView.findViewById(R.id.tv_artist_name)
+
+        fun bind(savedPiece: SavedPiece){
+            songName.text = savedPiece.songName
+            artistName.text = savedPiece.artist
+        }
+    }
 }
