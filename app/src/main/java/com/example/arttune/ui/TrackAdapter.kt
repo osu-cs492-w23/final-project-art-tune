6e0ccf24dc2c247749ff6c3c1428baf89321dbd4
Andrea Tongsak <atongsak@gmail.com>
2023-03-23 02:35:31 -0700

    Unformatted Track List appearing

--- app/src/main/java/com/example/arttune/ui/TrackAdapter.kt
+++ app/src/main/java/com/example/arttune/ui/TrackAdapter.kt
@@ -5,15 +5,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.adamratzman.spotify.models.Track
 import com.example.arttune.R
 import com.example.arttune.data.SpotifyTrack
 
 class TrackAdapter() : RecyclerView.Adapter<TrackAdapter.TrackViewHolder>() {
-    private var tracks = listOf<SpotifyTrack>()
+    private var tracks = listOf<Track>()
 
     override fun getItemCount() = tracks.size
 
-    fun updateTrackList(newList: List<SpotifyTrack>?) {
+    fun updateTrackList(newList: List<Track>?) {
         tracks = newList ?: listOf()
         notifyDataSetChanged()
     }
@@ -29,15 +30,15 @@ class TrackAdapter() : RecyclerView.Adapter<TrackAdapter.TrackViewHolder>() {
     }
 
     class TrackViewHolder (view: View) : RecyclerView.ViewHolder(view) {
-        private var currentTrack: SpotifyTrack? = null
+        private var currentTrack: Track? = null
         private val nameTV = view.findViewById<TextView>(R.id.tv_track_title)
         private val artistTV = view.findViewById<TextView>(R.id.tv_track_title)
         private val lengthTV = view.findViewById<TextView>(R.id.tv_track_length)
 
-        fun bind(track: SpotifyTrack) {
+        fun bind(track: Track) {
             currentTrack = track
-            nameTV.text = track.trackTitle
-            artistTV.text = track.artists
+            nameTV.text = track.name
+            artistTV.text = track.artists.toString()
             lengthTV.text = track.length.toString()
         }
     }
