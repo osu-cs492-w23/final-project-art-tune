5608a781a485a9efa913f882a002812813dc0e5f
dodsonta <71948722+dodsonta@users.noreply.github.com>
2023-03-23 22:35:25 -0700

    Got rid of placeholders and cleaned up some

--- app/src/main/java/com/example/arttune/ui/GalleryActivity.kt
+++ app/src/main/java/com/example/arttune/ui/GalleryActivity.kt
@@ -1,16 +1,13 @@
 package com.example.arttune.ui
 
 import android.os.Bundle
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.arttune.R
-import com.example.arttune.data.SavedPiece
 import com.spotify.sdk.android.auth.LoginActivity.REQUEST_CODE
-import java.util.jar.Manifest
 
 class GalleryActivity: AppCompatActivity() {
     private lateinit var galleryRV: RecyclerView
@@ -22,7 +19,6 @@ class GalleryActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gallery)
 
-        //ActivityResultContracts.RequestPermission()
         ActivityCompat.requestPermissions(this,
             Array(1) {android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
         REQUEST_CODE)
@@ -34,21 +30,6 @@ class GalleryActivity: AppCompatActivity() {
         galleryRV.adapter = galleryAdapter
         galleryAdapter.getViewModel(viewModel)
 
-        val entry1 = SavedPiece(
-            "In the Aeroplane Over the Sea", "Neutral Milk Hotel",
-            "Postcard", "Chris Bilheimer",
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/ItAotS_postcard_origins.jpg/200px-ItAotS_postcard_origins.jpg",
-            "placeholder"
-        )
-        val entry2 = SavedPiece(
-            "Walkin", "Denzel Curry",
-            "Young girl in a white dress walking on Skagen Sønderstrand",
-            "Michael Peter Ancher",
-            "https://i.pinimg.com/originals/56/fe/97/56fe9724beef7c3e15f9b88928d18e3f.jpg",
-            "Placeholder"
-        )
-        viewModel.addSavedPiece(entry1)
-        viewModel.addSavedPiece(entry2)
         viewModel.savedPieces.observe(this){savedPieces ->
             galleryAdapter.updateGallery(savedPieces)
         }
